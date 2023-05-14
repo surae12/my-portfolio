dc29a10a3be384d6407c99b33bbf420ef8511442
surae12 <suricekmar12@gmail.com>
2023-05-14 15:55:05 +0530

    responsive changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { useRef } from 'react';
 import { Link } from 'react-scroll'
-
+import { FaBars, FaTimes } from "react-icons/fa";
 import "./Header.css"
 const Header = () => {
+
+    const navRef = useRef();
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+
+    }
     return (
         <div className='header'>
             <div className='header_left'><h1>Suresh Kumar S</h1></div>
-            <div className='header_right'>
+            <div className='header_right' ref={navRef}>
                 <Link to="about" smooth={true} duration={500}>
                     <h4>About Me</h4>
                 </Link>
@@ -19,8 +26,9 @@ const Header = () => {
                 <Link to="contact" smooth={true} duration={500}>
                     <h4>Contact Me</h4>
                 </Link>
-
+                <button className='nav-btn nav-close-btn' onClick={showNavbar}><FaTimes /></button>
             </div>
+            <button className='nav-btn' onClick={showNavbar}><FaBars /></button>
         </div>
     )
 }
